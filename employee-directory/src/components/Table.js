bb122b8163dc244fc0fa80066397894262ef6c7b
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-06 14:51:30 -0400

    made a mock table

---
+++ employee-directory/src/components/Table.js
@@ -0,0 +1,36 @@
+import React from "react";
+
+function Table() {
+    return(
+        <div className="table-responsive">
+            <table className="table">
+                <thead>
+                    <tr>
+                        <th scope="col">ID</th>
+                        <th scope="col">First Name</th>
+                        <th scope="col">Last Name</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <th scope="row">1</th>
+                        <td>Michael</td>
+                        <td>Scott</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">2</th>
+                        <td>Dwight</td>
+                        <td>Schrute</td>
+                    </tr>
+                    <tr>
+                        <th scope="row">3</th>
+                        <td>Jim</td>
+                        <td>Halpert</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    );
+}
+
+export default Table;
