41c66fc485f73bb010fb0d3569664e130e46ca6d
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-10 15:09:20 -0400

    changing table format

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Jumbotron from "./components/Jumbotron";
-import Navbar from "./components/Navbar";
 import Table from "./components/Table";
 
 function App() {
     return (
         <div>
             <Jumbotron />
-            <Navbar />
             <Table />
         </div>
 
