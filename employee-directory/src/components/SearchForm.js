f65559431e3e7682f1086a7c6056229fd0e67cd1
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-10 20:54:07 -0400

    made a list of employees to search for

---
+++ employee-directory/src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const employees = [
+    "Dwight Schrute",
+    "Jim Halpert",
+    "Pam Beesly",
+    "Michael Scott",
+    "Ryan Howard",
+    "Kelly Kapoor",
+    "Creed Bratton",
+    "Angela Martin"
+]
