0d5fa07cd06bd0c1f351bd64a5d3f3095ffcd1af
Nathan Sanford <nwsanfor24@gmail.com>
2020-05-10 20:51:51 -0400

    starting search form from scratch

--- employee-directory/src/App.js
+++ employee-directory/src/App.js
@@ -1,13 +1,11 @@
 import React from "react";
 import Jumbotron from "./components/Jumbotron";
-import SearchForm from "./components/SearchForm";
 import Table from "./components/Table";
 
 function App() {
     return (
         <div>
             <Jumbotron />
-            <SearchForm />
             <Table     />
         </div>
 
